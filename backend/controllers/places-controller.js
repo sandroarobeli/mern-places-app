64ece5ee336436e24f0f39450ac85ab92e827d1e
Alex Arobelidze <sandroarobeli77@gmail.com>
2021-08-30 21:43:50 -0500

    completed places model and integrated with places-controllers. (CRUD)

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -4,51 +4,49 @@ const { validationResult } = require('express-validator')
 // Custom modules
 const HttpError = require('../models/http-error')
 const getCoordinates = require('../utility/location')
+const Place = require('../models/place-model')
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous buildings in the world',
-        location: {
-            lat: 40.7484474,
-            lng: -73.9871516
-        },
-        address: '20 W 34th st, New York, NY, 10001',
-        creator: 'u1'
-    }
-]
 
 // List the place by its ID
-const getPlaceById  = (req, res, next) => {
+const getPlaceById  = async (req, res, next) => {
     const placeId = req.params.placeId
-    const place = DUMMY_PLACES.find(place => place.id === placeId) // temporary
+    try {
+        const place = await Place.findById(placeId)
+        if (!place) {
+            // IN SYNCHRONOUS MODE USE next(error) OR throw new Error('some generic message'),
+            // IN A-SYNCHRONOUS MODE ONLY USE return next(error)
+            return next(new HttpError(`Place with ID: ${placeId} not found`, 404))
+        }
+        // adds id property (in addition to _id) to the returned Object
+        res.status(200).json({ place: place.toObject({ getters: true }) }) 
+    } catch (error) {
+        return next(new HttpError(`Unable to retrieve Place: ${error.message}`, 500))
+    }    
     
-    if (!place) {
-        // IN SYNCHRONOUS MODE USE next(error) OR throw new Error('some generic message'),
-        // IN A-SYNCHRONOUS MODE ONLY USE return next(error)
-        return next(new HttpError(`Place with ID: ${placeId} not found`, 404))
-    }
-    res.json({ place })
 }
 
 // List all places created by a given user
-const getPlacesByUserId = (req, res, next) => {
+const getPlacesByUserId = async (req, res, next) => {
     const userId = req.params.userId
-    const places = DUMMY_PLACES.filter(place => place.creator === userId)
-    if (!places || places.length === 0) {
-        // IN SYNCHRONOUS MODE USE next(error) OR throw new Error('some generic message'),
-        // IN A-SYNCHRONOUS MODE ONLY USE return next(error)
-        return next(new HttpError(`No places found for user ${userId}`, 404))
+    try {
+        const places = await Place.find({ creator: userId })
+        if (!places || places.length === 0) {
+            // IN SYNCHRONOUS MODE USE next(error) OR throw new Error('some generic message'),
+            // IN A-SYNCHRONOUS MODE ONLY USE return next(error)
+            return next(new HttpError(`No places found for user ${userId}`, 404))
+        }
+        // adds id property (in addition to _id) to the returned Object(s)
+        res.status(200).json({ places: places.map(place => place.toObject({ getters: true })) })
+    } catch (error) {
+        return next(new HttpError(`Unable to retrieve places: ${error.message}`, 500))
     }
-    
-    res.json({ places })
 }
 
 // Create a new Place
 const createPlace = async (req, res, next) => {
     // Middleware registered in the routes gets invoked here
-    // If returned errors object isn't empty, error is passed down the chain via next() 
+    // If returned errors object isn't empty, error is passed down the chain via next()
+    // THIS TRY-CATCH ENSURES PROCESSING OF INPUT PROPERTIES 
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         console.log(errors.errors)//test
@@ -56,9 +54,10 @@ const createPlace = async (req, res, next) => {
     }
 
     // Getting manually entered properties from the user request  
-    const { title, description, address, creator } = req.body 
+    const { title, description, image, address, creator } = req.body 
     
     // Getting coordinates by using geocoding function
+    // THIS TRY-CATCH ENSURES PROCESSING OF ADDRESS --> COORDINATES CONVERSION
     let coordinates  
     try {
         coordinates = await getCoordinates(address)
@@ -67,19 +66,26 @@ const createPlace = async (req, res, next) => {
     }
 
     // combining all of above to create a new place
-    const createdPlace = {
+    const createdPlace = new Place({
         title,
         description,
+        image,
         location: coordinates,
         address,
         creator
+    })
+    // THIS TRY-CATCH ENSURES PROPER NETWORK PROTOCOL EXCHANGE
+    try {
+        await createdPlace.save()
+        res.status(201).json({ place: createdPlace.toObject({ getters: true }) })   
+    } catch (error) {
+        return next(new HttpError(`Creating Place failed: ${error.message}`, 500))
     }
-    DUMMY_PLACES.push(createdPlace)
-    res.status(201).json({ place: createdPlace })   
+    
 }
 
 // Update place
-const updatePlaceById = (req, res, next) => {
+const updatePlaceById = async (req, res, next) => {
     // Middleware registered in the routes gets invoked here
     // If returned errors object isn't empty, error is passed down the chain via next() 
     const errors = validationResult(req)
@@ -91,26 +97,34 @@ const updatePlaceById = (req, res, next) => {
     
     const placeId = req.params.placeId
     const { title, description } = req.body
-    const updatedPlace = {...DUMMY_PLACES.find(place => place.id === placeId)}
-    const placeIndex = DUMMY_PLACES.findIndex(place => place.id === placeId)
-    updatedPlace.title = title
-    updatedPlace.description = description
-
-    DUMMY_PLACES[placeIndex] = updatedPlace
-    res.status(200).json({ place: updatedPlace })
+    
+    try {
+        const updatedPlace = await Place.findByIdAndUpdate(
+            placeId,
+            { title, description },
+            { new: true }
+        )
+        if (!updatedPlace) {
+            return next(new HttpError(`Place with ID: ${placeId} not found`, 404))
+        }
+        res.status(200).json({ place: updatedPlace.toObject({ getters: true }) })
+    } catch (error) {
+        return next(new HttpError(`Updating Place failed: ${error.message}`, 500))
+    }
 }
 
 // delete Place
-const deletePlaceById = (req, res, next) => {
+const deletePlaceById = async (req, res, next) => {
     const placeId = req.params.placeId
-    // Place with given ID exists to begin with
-    const placeExists = DUMMY_PLACES.find(place => place.id === placeId)
-    if (!placeExists) {
-        return next(new HttpError(`Place with ID ${placeId} not found`, 404))
+    try {
+        const deletedPlace = await Place.findByIdAndDelete(placeId)
+        if (!deletedPlace) {
+            return next(new Error(`Place with ID: ${placeId} not found`, 404))
+        }
+        res.status(200).json({ message: `${deletedPlace.title} has been successfully deleted`})
+    } catch (error) {
+        return next(new HttpError(`Deleting Place failed: ${error.message}`, 500))
     }
-    // If so, delete it by filtering out of the array
-    DUMMY_PLACES.filter(place => place.id !== placeId)
-    res.status(200).json({ message: `Place with ID ${placeId} has been deleted`})
 }
 
 exports.getPlaceById = getPlaceById
